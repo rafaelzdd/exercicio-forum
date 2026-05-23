a5864ee94cfcdbd6c267063bc15bae84025a945d
Rafael Pereira <92816440+rafaelzdd@users.noreply.github.com>
2026-05-23 18:05:43 +0000

    atualiza o `index.js`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -39,8 +39,6 @@ export default async function migrations(request, response) {
       }
       return response.status(200).json(migratedMigrations);
     }
-
-    return response.status(405).end();
   } catch (error) {
     console.error(error);
     throw error;
